15343afe9e9b7d8a72c09abf83ae793c6142abe3
Jonathon Moody <jonathonm12@yahoo.com>
2023-09-15 09:28:38 -0400

    Mostly done with iconinput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,64 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const SIZES = {
+  small: '16px',
+  large: '24px',
 };
+
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const elementSize = SIZES[size];
+
+  return (
+    <Wrapper for="search">
+      <IconWrapper>
+        <Icon size={elementSize} id={icon} />
+      </IconWrapper>
+      <VisuallyHidden>
+        <label for="search">{label}</label>
+      </VisuallyHidden>
+      <TextInput
+        type="text"
+        id="search"
+        name="search"
+        placeholder={placeholder}
+        width={width}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+  display: flex;
+  gap: 10px;
+  border-bottom: 2px solid black;
+  outline-offset: 2px;
+
+  &:focus-within {
+    outline: solid 2px black;
+  }
+`;
+
+const IconWrapper = styled.div`
+  ${Wrapper}:hover & > div > svg {
+    stroke-width: 2px;
+  }
+`;
+
+const TextInput = styled.input`
+  width: ${(props) => props.width}px;
+  border: none;
+  outline: none;
+
+  ${Wrapper}:hover & {
+    font-weight: 700;
+  }
+
+  ${Wrapper}:hover &::placeholder {
+    font-weight: normal;
+  }
+`;
 
 export default IconInput;
