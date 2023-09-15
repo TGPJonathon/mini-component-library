20a09fab10e4970eb82139232426e31291f77440
Jonathon Moody <jonathonm12@yahoo.com>
2023-09-15 08:20:34 -0400

    After many trials and tribulations, made a select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,62 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <InvisibleSelect
+        aria-controls="listbox1"
+        aria-expanded="false"
+        aria-haspopup="listbox"
+        role="combobox"
+        value={value}
+        onChange={onChange}
+      >
+        {children}
+      </InvisibleSelect>
+      <StyledSelect
+        tabIndex={0}
+        color={COLORS.transparentGray15}
+        textColor={COLORS.gray700}
+      >
+        {displayedValue} <Icon id="chevron-down" strokeWidth={2} />
+      </StyledSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const InvisibleSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+`;
+
+const StyledSelect = styled.div`
+  color: ${(props) => props.textColor};
+  background-color: ${(props) => props.color};
+  border-radius: 8px;
+  padding: 12px 16px;
+  border: none;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  ${InvisibleSelect}:focus + & {
+    border: solid 2px black;
+  }
+
+  ${InvisibleSelect}:hover + & {
+    color: black;
+  }
+`;
 
 export default Select;
